85b6fd8ff1dcfd218d8a1094da882835411262ad
Damian Tarnawsky <damian@ionic.io>
2023-03-13 07:46:50 -0700

    Initial Commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,123 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Preferences } from '@capacitor/preferences';
+
+import {
+  IntuneMAM, IntuneMAMAppConfig, IntuneMAMGroupName,
+  IntuneMAMPolicy, IntuneMAMUser, IntuneMAMVersionInfo
+} from '@ionic-enterprise/intune';
+import { BehaviorSubject } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  tokenInfo: any = null;
+  statusMsg = '';
+  user$: BehaviorSubject<IntuneMAMUser | null> = new BehaviorSubject(null);
+  version: IntuneMAMVersionInfo | null = null;
+  groupName: IntuneMAMGroupName | null = null;
+  appConfig: IntuneMAMAppConfig | null = null;
+  policy: IntuneMAMPolicy | null = null;
+  user: IntuneMAMUser;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.user$.subscribe(async user => {
+      console.log('User changed', user);
+      if (user?.upn) {
+        this.appConfig = await IntuneMAM.appConfig(user);
+        this.groupName = await IntuneMAM.groupName(user);
+        this.policy = await IntuneMAM.getPolicy(user);
+
+        await this.getToken();
+      }
+      this.user = user;
+    });
+
+    setTimeout(async () => {
+      const v = await Preferences.get({ key: 'unenrollStatus' });
+      this.statusMsg = v.value;
+      console.log('this.statusMsg',v.value);
+    }, 5000);
+  }
+
+  async ionViewDidEnter() {
+    this.version = await IntuneMAM.sdkVersion();
+
+    this.user$.next(await IntuneMAM.enrolledAccount());
+
+    IntuneMAM.addListener('appConfigChange', () => {
+      console.log('Policy change here');
+    });
+
+    IntuneMAM.addListener('policyChange', () => {
+      console.log('Policy change here');
+    });
+  }
+
+  async showConsole() {
+    await IntuneMAM.displayDiagnosticConsole();
+  }
+
+  async status(value: string) {
+    await Preferences.set({
+      key: 'unenrollStatus',
+      value,
+    });
+  }
+  async unenroll() {
+    await this.status('starting....');
+    await IntuneMAM.deRegisterAndUnenrollAccount(this.user$.value);
+    await this.status('done');
+    const user = await IntuneMAM.enrolledAccount();
+    await this.status(JSON.stringify(user));
+    alert('Enrolled User says: ' + JSON.stringify(user));
+  }
+  async getToken() {
+    if (this.user$.value?.upn) {
+      try {
+        const tokenInfo = await IntuneMAM.acquireTokenSilent({
+          scopes: ['https://graph.microsoft.com/.default'],
+          upn: this.user$.value.upn
+        });
+        this.tokenInfo = tokenInfo;
+        console.log('Got token info', tokenInfo);
+      } catch (error) {
+        console.error(
+          `Unable to silently acquire token: ${error}, getting interactive`
+        );
+        try {
+          const tokenInfo = await IntuneMAM.acquireToken({
+            scopes: ['https://graph.microsoft.com/.default'],
+          });
+          this.tokenInfo = tokenInfo;
+        } catch (atError) {
+          alert(`Unable to acquire token ${atError}`);
+        }
+      }
+    }
+  }
+
+  async logout() {
+    if (this.user$.value) {
+      await IntuneMAM.deRegisterAndUnenrollAccount(this.user$.value);
+    }
+    this.router.navigate(['/']);
+  }
+
+  getTokenInfoJson() {
+    return JSON.stringify((this.tokenInfo || {}), null, 2);
+  }
+
+  getPolicyJson() {
+    return JSON.stringify((this.policy || {}), null, 2);
+  }
+
+  getAppConfigJson() {
+    return JSON.stringify((this.appConfig || {}), null, 2);
+  }
+}
